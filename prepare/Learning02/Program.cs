c3e655423fa39ae75f4034122f0fb06833a74c75
mrjoll3y <callen.jolley7@gmail.com>
2024-09-23 21:28:33 -0600

    entire project with no errors

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Team Lead";
+        job1._company = "Walmart";
+        job1._yearStart = "2023";
+        job1._yearLast = "2026";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Team Lead / Trainer";
+        job2._company = "Chick-fil-A";
+        job2._yearStart = "2020";
+        job2._yearLast = "2022";
+
+        Resume myResume = new Resume();
+        myResume._name = "Callen Jolley";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
+    
 }
---
+++ prepare/Learning02/job.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+public class Job 
+{
+    public string _jobTitle;
+    public string _company;
+    public string _yearStart;
+    public string _yearLast;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_jobTitle} ({_company}) {_yearStart}-{_yearLast}");
+    }
+}
