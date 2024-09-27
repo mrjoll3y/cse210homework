a72c4e3a9a2d5d53af4c27769a05ea696274e41e
mrjoll3y <callen.jolley7@gmail.com>
2024-09-26 21:02:16 -0600

    end of video segment

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal theJournal = new Journal();
+        Entry anEntry = new Entry();
+        anEntry.Display();
     }
 }
