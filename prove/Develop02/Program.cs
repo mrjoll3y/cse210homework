524d0cec08542baf229082d40722a802342c7374
mrjoll3y <callen.jolley7@gmail.com>
2024-09-28 17:52:51 -0600

    fixed errors when running debug

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,8 +7,6 @@ public class Program
         "What was the best part of my day?",
         "How did I see the hand of the Lord in my life today?",
         "What was the strongest emotion I felt today?",
-        "If I had one thing I could do over today, what would it be?"
-        "What made you smile today?",
         "What are you grateful for right now?",
         "Describe a moment when you felt at peace today",
         "What was the most surprising thing that happened today?",
