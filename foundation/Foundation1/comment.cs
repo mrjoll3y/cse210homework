35a3085ad6d4196796b14309c8a0d18a31c7058b
mrjoll3y <callen.jolley7@gmail.com>
2024-10-10 09:47:10 -0600

    working prgram wirh no errors, system wasn't updating the errors until I ran the program

--- foundation/Foundation1/comment.cs
+++ foundation/Foundation1/comment.cs
@@ -1,11 +1,11 @@
 using System;
 class Comment
 {
-    // Properties for the commenter's name and comment text
+    
     public string Name { get; private set; }
     public string Text { get; private set; }
 
-    // Constructor to initialize comment details
+    
     public Comment(string name, string text)
     {
         Name = name;
--- foundation/Foundation1/video.cs
+++ foundation/Foundation1/video.cs
@@ -1,15 +1,15 @@
 using System;
 class Video
 {
-    // Properties for video title, author, and length in seconds
+    
     public string Title { get; private set; }
     public string Author { get; private set; }
     public int Length { get; private set; }
 
-    // List to hold comments for the video
+    
     public List<Comment> Comments { get; private set; }
 
-    // Constructor to initialize the video details
+    
     public Video(string title, string author, int length)
     {
         Title = title;
@@ -18,13 +18,13 @@ class Video
         Comments = new List<Comment>();
     }
 
-    // Method to add a comment to the video
+    
     public void AddComment(Comment comment)
     {
         Comments.Add(comment);
     }
 
-    // Method to get the number of comments
+    
     public int GetNumberOfComments()
     {
         return Comments.Count;
