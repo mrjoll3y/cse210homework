9079723b32de9edba28846b968b3de74b57ae68b
mrjoll3y <callen.jolley7@gmail.com>
2024-10-10 09:38:19 -0600

    dfvb

---
+++ foundation/Foundation1/comment.cs
@@ -0,0 +1,14 @@
+using system;
+class Comment
+{
+    // Properties for the commenter's name and comment text
+    public string Name { get; private set; }
+    public string Text { get; private set; }
+
+    // Constructor to initialize comment details
+    public Comment(string name, string text)
+    {
+        Name = name;
+        Text = text;
+    }
+}
