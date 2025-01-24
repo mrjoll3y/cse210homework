8a54d89d7a85be88ade535c952688379b97d1f39
mrjoll3y <callen.jolley7@gmail.com>
2025-01-24 16:49:59 -0700

    dwcvsfb

--- prepare/Learning02/task.cs
+++ prepare/Learning02/task.cs
@@ -7,6 +7,6 @@ public class Task
         public string Title { get; set; }
         public DateTime DueDate { get; set; }
 
-        public abstract void DisplayTask();
+        public void DisplayTask();
     }
 
